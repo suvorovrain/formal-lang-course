7a62b7ee1b377a3e8e564c1370e935013049665b
Rodion Suvorov <rodion.suvorov.94@mail.ru>
2025-09-07 09:35:03 +0300

    refactor: apply formatter

--- project/graph_utils.py
+++ project/graph_utils.py
@@ -2,11 +2,13 @@ import cfpq_data as cfpq
 from networkx.drawing.nx_pydot import to_pydot
 from dataclasses import dataclass
 
+
 @dataclass
 class GraphInfo:
     nodes_number: int
     edges_number: int
     labels: list
+
 
 def get_graph_info(name):
     graph_path = cfpq.download(name)
@@ -16,7 +18,10 @@ def get_graph_info(name):
     labels = cfpq.get_sorted_labels(graph)
     return GraphInfo(n_nodes, n_edges, labels)
 
+
 def save_two_cycles_graph(fst_cycle_node_number, snd_cycle_node_number, nodes, path):
-    graph = cfpq.labeled_two_cycles_graph(m = fst_cycle_node_number,n = snd_cycle_node_number,labels = nodes)
+    graph = cfpq.labeled_two_cycles_graph(
+        m=fst_cycle_node_number, n=snd_cycle_node_number, labels=nodes
+    )
     dot_graph = to_pydot(graph)
     dot_graph.write_raw(path)
--- tests/test_task1.py
+++ tests/test_task1.py
@@ -1,15 +1,16 @@
-import pytest
 from project.graph_utils import get_graph_info, save_two_cycles_graph
 import os
+
 
 def test_get_graph_info():
     graph_info = get_graph_info("bzip")
     assert graph_info.nodes_number == 632
     assert graph_info.edges_number == 556
-    assert graph_info.labels == ['d','a']
+    assert graph_info.labels == ["d", "a"]
+
 
 def test_save_two_cycles_graph():
-    save_two_cycles_graph(5,5,("a","d"),"tests/test_task1_result.dot")
+    save_two_cycles_graph(5, 5, ("a", "d"), "tests/test_task1_result.dot")
     try:
         with open("tests/testdata/test_task1.dot", "r") as expected_graph:
             with open("tests/test_task1_result.dot", "r") as result_graph:
