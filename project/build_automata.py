9e438940afc07641a575ff3b89cf30a8e9791045
Rodion Suvorov <rodion.suvorov.94@mail.ru>
2025-09-13 17:35:54 +0300

    feat: implement automata builder functions

--- project/build_automata.py
+++ project/build_automata.py
@@ -1,11 +1,34 @@
 from networkx import MultiDiGraph
-from pyformlang.finite_automaton import DeterministicFiniteAutomaton,NondeterministicFiniteAutomaton
+from pyformlang.finite_automaton import (
+    DeterministicFiniteAutomaton,
+    NondeterministicFiniteAutomaton,
+    State,
+)
 from typing import Set
+import pyformlang.regular_expression
+
 
 def regex_to_dfa(regex: str) -> DeterministicFiniteAutomaton:
-    pass
+    return (
+        pyformlang.regular_expression.Regex(regex).to_epsilon_nfa().to_deterministic()
+    )
+
 
 def graph_to_nfa(
-  graph: MultiDiGraph, start_states: Set[int], final_states: Set[int]
+    graph: MultiDiGraph, start_states: Set[int], final_states: Set[int]
 ) -> NondeterministicFiniteAutomaton:
-    pass
+    nfa = NondeterministicFiniteAutomaton.from_networkx(graph)
+    if len(start_states) > 0:
+        for s in start_states:
+            nfa.add_start_state(State(s))
+    else:
+        for v in graph.nodes:
+            nfa.add_start_state(State(v))
+
+    if len(final_states) > 0:
+        for s in final_states:
+            nfa.add_final_state(State(s))
+    else:
+        for v in graph.nodes:
+            nfa.add_final_state(State(v))
+    return nfa
