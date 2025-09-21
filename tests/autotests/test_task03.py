510b16776f9019227543b5119e85bfdbbbcbd978
Rodion Suvorov <rodion.suvorov.94@mail.ru>
2025-09-21 23:25:18 +0300

    fix: correct module name in tests

--- tests/autotests/test_task03.py
+++ tests/autotests/test_task03.py
@@ -9,11 +9,12 @@ import random
 import itertools
 from grammars_constants import REGEXES
 from rpq_concrete_cases import CASES_RPQ, CaseRPQ
+import sys
 
 # Fix import statements in try block to run tests
 try:
-    from project.task2 import regex_to_dfa
-    from project.task3 import intersect_automata, AdjacencyMatrixFA, tensor_based_rpq
+    from project.build_automata import regex_to_dfa
+    from project.rpq_any import intersect_automata, AdjacencyMatrixFA, tensor_based_rpq
 except ImportError:
     pytestmark = pytest.mark.skip("Task 3 is not ready to test!")
 
